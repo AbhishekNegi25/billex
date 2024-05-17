d055ecc982b6a28d743f133c9d8638777b5349d5
AbhishekNegi25 <abhishek.negi@coloredcow.in>
2024-05-17 23:27:37 +0530

    connected with the data base

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :billex, Billex.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "negi",
+  password: nil,
   hostname: "localhost",
   database: "billex_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
